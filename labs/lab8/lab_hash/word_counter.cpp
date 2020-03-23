ff0dc2801d06aae773e588711596e1e4b0b70510
Mohamed <mabouzadi642@gmail.com>
2020-03-22 19:29:14 -0700

    hash the number of words in a file

--- labs/lab8/lab_hash/word_counter.cpp
+++ labs/lab8/lab_hash/word_counter.cpp
@@ -27,13 +27,26 @@ vector<pair<string, int>> WordFreq<Dict>::getWords(int threshold) const
 {
     TextFile infile(filename);
     vector<pair<string, int>> ret;
+    //vector<pair<string, int>> temp;
     /**
      * @todo Implement this function.
      * @see char_counter.cpp if you're having trouble.
      */
+    Dict<string, int> temp(100);
 
+    while (infile.good()) {
+        string word = infile.getNextWord();
+        temp[word]++;
+    }
 
-    (void) threshold; // prevent warnings... When you implement this function, remove this line.
+    typename Dict<string, int>::iterator it;
+    for (it = temp.begin(); it != temp.end(); it++) {
+        if (it->second >= threshold) {
+            ret.push_back(*it);
+        }
+    }
+
+    //(void) threshold; // prevent warnings... When you implement this function, remove this line.
 
     return ret;
 }
